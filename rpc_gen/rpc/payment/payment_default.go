db2202984404bba0f1191221b636d56aab48a468
Zhiyu Shuai <165612681+peaceouty@users.noreply.github.com>
2025-02-24 23:33:06 +0800

    rpc_gen

---
+++ rpc_gen/rpc/payment/payment_default.go
@@ -0,0 +1,35 @@
+package payment
+
+import (
+	"context"
+	payment "github.com/cloudwego/biz-demo/gomall/rpc_gen/kitex_gen/payment"
+	"github.com/cloudwego/kitex/client/callopt"
+	"github.com/cloudwego/kitex/pkg/klog"
+)
+
+func Charge(ctx context.Context, req *payment.ChargeReq, callOptions ...callopt.Option) (resp *payment.ChargeResp, err error) {
+	resp, err = defaultClient.Charge(ctx, req, callOptions...)
+	if err != nil {
+		klog.CtxErrorf(ctx, "Charge call failed,err =%+v", err)
+		return nil, err
+	}
+	return resp, nil
+}
+
+func CancelPayment(ctx context.Context, req *payment.CancelPaymentReq, callOptions ...callopt.Option) (resp *payment.CancelPaymentResp, err error) {
+	resp, err = defaultClient.CancelPayment(ctx, req, callOptions...)
+	if err != nil {
+		klog.CtxErrorf(ctx, "CancelPayment call failed,err =%+v", err)
+		return nil, err
+	}
+	return resp, nil
+}
+
+func ScheduleCancelPayment(ctx context.Context, req *payment.ScheduleCancelPaymentReq, callOptions ...callopt.Option) (resp *payment.ScheduleCancelPaymentResp, err error) {
+	resp, err = defaultClient.ScheduleCancelPayment(ctx, req, callOptions...)
+	if err != nil {
+		klog.CtxErrorf(ctx, "ScheduleCancelPayment call failed,err =%+v", err)
+		return nil, err
+	}
+	return resp, nil
+}
